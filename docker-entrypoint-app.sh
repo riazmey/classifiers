441eaeb9acc042c36187d92c81cf398d75f37a89
Иван Радаев <i.radaev@ltlog.ru>
2025-04-23 11:06:16 +0300

    Edit entry-point

--- docker-entrypoint-app.sh
+++ docker-entrypoint-app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Apply database migrations
-python manage.py makemigrations
-python manage.py migrate
+#python manage.py makemigrations
+#python manage.py migrate
 
 # Collect static files
 #python manage.py collectstatic --noinput
