c1c8fd56cc94242b23525411a3e883b84bc6d9bc
Иван Радаев <i.radaev@ltlog.ru>
2025-04-23 16:28:15 +0300

    Edit entrypoint app

--- docker-entrypoint-app.sh
+++ docker-entrypoint-app.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 # Apply database migrations
-python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
-#python manage.py collectstatic --noinput
-
-export DJANGO_SETTINGS_MODULE=core.settings
+python manage.py collectstatic --noinput
 
 # Start server
 python manage.py runserver "0.0.0.0:${WEB_PORT}"
