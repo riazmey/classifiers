60f1183ffa1db7dbfb41097878283f02f1970e9e
Иван Радаев <i.radaev@ltlog.ru>
2025-04-23 12:30:09 +0300

    Add coments

--- core/settings/__init__.py
+++ core/settings/__init__.py
@@ -1,10 +1,14 @@
 
 from .main import *
 
+print('settings.main.py - enabled')
+
 try:
     from .local import *
+    print('settings.local.py - enabled')
 except ImportError:
     from .environ import *
+    print('settings.environ.py - enabled')
 
 if DEBUG:
     print('Debug view environments vars:')
