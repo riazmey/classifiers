b187ef5da9d96ed06a2e27cc9b54ee2178da2f97
Иван Радаев <i.radaev@ltlog.ru>
2025-04-23 10:20:05 +0300

    Edit environment for docker

--- docker-entrypoint-app.sh
+++ docker-entrypoint-app.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
 # Apply database migrations
-#echo "Apply database migrations"
 python manage.py makemigrations
 python manage.py migrate
 
 # Collect static files
-#echo "Collect static files"
 #python manage.py collectstatic --noinput
 
-export DJANGO_SETTINGS_MODULE=core.settings
+#export DJANGO_SETTINGS_MODULE=core.settings
 
 # Start server
-echo "Starting server"
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:${WEB_PORT}
