8fce805240902b1a4028bc520b8e2d63bbe33ca1
Иван Радаев <i.radaev@ltlog.ru>
2025-05-06 13:18:15 +0300

    Edit entrypoint app

--- entrypoint-app.sh
+++ entrypoint-app.sh
@@ -1,8 +1,8 @@
 
 #!/bin/sh
 
-python manage.py createcachetable
+python /app/classifiers/manage.py createcachetable
 
-#python /app/classifiers/manage.py runserver 0.0.0.0:${WEB_PORT}
+python /app/classifiers/manage.py runserver 0.0.0.0:${WEB_PORT}
 #gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
-gunicorn server.wsgi --bind 0.0.0.0:${WEB_PORT} --workers 6 --threads 6
+#gunicorn server.wsgi --bind 0.0.0.0:${WEB_PORT} --workers 6 --threads 6
