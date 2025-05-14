10501e67fdc43fcd0e4e389b6029c685faa0fab8
Иван Радаев <i.radaev@ltlog.ru>
2025-05-14 14:28:13 +0300

    edit apps

--- classifiers/apps.py
+++ classifiers/apps.py
@@ -5,8 +5,4 @@ from django.apps import AppConfig
 class ClassifiersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "classifiers"
-<<<<<<< HEAD
-    verbose_name = 'Сервис "Классификаторы"'
-=======
     verbose_name = 'Сервис "Общероссийских классификаторов"'
->>>>>>> main
